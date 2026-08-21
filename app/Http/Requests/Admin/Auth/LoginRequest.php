765406b0869a85b71b6a804b979a82e06c86628b
Martin DELEBECQUE <delebecque.martin@gmail.com>
2026-08-21 21:06:57 +0200

    Refactor password hashing to use dynamic configuration for improved flexibility

--- app/Http/Requests/Admin/Auth/LoginRequest.php
+++ app/Http/Requests/Admin/Auth/LoginRequest.php
@@ -66,7 +66,7 @@ class LoginRequest extends FormRequest
                 'email' => [__('auth.failed')],
             ]);
         }
-        $hash = \Hash::driver('bcrypt');
+        $hash = \Hash::driver(setting('hash_driver', 'bcrypt'));
         if ($hash->check($this->input('password'), $admin->password) && ($admin->expires_at == null || $admin->expires_at > now())) {
             if (\Hash::needsRehash($admin->password)) {
                 $admin->forceFill(['password' => \Hash::make($this->input('password'))])->save();
