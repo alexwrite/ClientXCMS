acbc378a612c832a4335afe9ec4bafa1c0569d2a
Martin DELEBECQUE <delebecque.martin@gmail.com>
2026-08-26 10:29:49 +0200

    Bump version to 2.17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    const VERSION = '2.16';
+    const VERSION = '2.17';
 
     /**
      * Register any application services.
